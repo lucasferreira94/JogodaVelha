3bf76539b346845239f988d8dbdfc1ea3559c5bf
lucasferreira94 <lucas_sferreira94@hotmail.com>
2022-06-18 13:03:03 -0300

    criacao das funcoes

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-def CriarPlataforma():
+def inserirLetra(letra, posicao):
     pass
+
+def espacoVazio(posicao):
+    pass
+
+def desenharPlataforma(board):
+    pass
+
+def Vencedor(bo, le):
+    pass
+
+def movJogador():
+    pass
+
+def compMove():
+    pass
+
+def selecionaRandom(board):
+    pass
+
+def plataformaFull(board):
+    pass
+
+def main():
+    pass
+
+main()
