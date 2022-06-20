26fd809ff90133de0ea2f28364c1eed520a0b6ed
lucasferreira94 <lucas_sferreira94@hotmail.com>
2022-06-20 08:55:41 -0300

    problemas de identação e sintaxe arrumados

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 plataforma = [' ' for x in range(10)]
 
 
-def inserirLetra(letra, posicao):
+def inserirletra(letra, posicao):
     plataforma[posicao] = letra
 
-def espacoVazio(posicao):  # ESPAÇO DA PLATAFORMA ESTÁ VAZIO?
+
+def espacovazio(posicao):  # ESPAÇO DA PLATAFORMA ESTÁ VAZIO?
     return plataforma[posicao] == ' '
 
-def desenharPlataforma(plataforma):
+
+def desenharplataforma(plataforma):
     print('   |   |')
-    print(' ' + plataforma[1] + ' | ' + plataforma[2] + ' | ' + plataforma[3] )
+    print(' ' + plataforma[1] + ' | ' + plataforma[2] + ' | ' + plataforma[3])
     print('   |   |')
     print('-----------')
     print('   |   |')
@@ -21,30 +23,40 @@ def desenharPlataforma(plataforma):
     print(' ' + plataforma[7] + ' | ' + plataforma[8] + ' | ' + plataforma[9])
     print('   |   |')
 
-def Vencedor(pltfm, ltr):  # PLTFM E LTR ESTÃO ASSOCIADOS A PLATAFORMA E LETRA PARA CHECAR O VENCEDOR
-    return (pltfm[7] == ltr and pltfm[8] == ltr and pltfm[9] == ltr) or \  # A SEQUÊNCIA DA POSIÇÃO 7 8 E 9 DA PLATAFORMA ESTÁ COM AS MESMAS LETRAS?
-           (pltfm[4] == ltr and pltfm[5] == ltr and pltfm[6] == ltr) or \
-           (pltfm[1] == ltr and pltfm[2] == ltr and pltfm[3] == ltr) or \
-           (pltfm[1] == ltr and pltfm[4] == ltr and pltfm[7] == ltr) or \
-           (pltfm[2] == ltr and pltfm[5] == ltr and pltfm[8] == ltr) or \
-           (pltfm[3] == ltr and pltfm[6] == ltr and pltfm[9] == ltr) or \
-           (pltfm[1] == ltr and pltfm[5] == ltr and pltfm[9] == ltr) or \
-           (pltfm[3] == ltr and pltfm[5] == ltr and pltfm[7] == ltr) or
+
+# PLTFM E LTR ESTÃO ASSOCIADOS A PLATAFORMA E LETRA PARA CHECAR O VENCEDOR
+# A SEQUÊNCIA DA POSIÇÕES {X Y Z} DA PLATAFORMA ESTÃO COM AS MESMAS LETRAS?
 
 
-def movJogador():
+def vencedor(pltfm, ltr):
+    return (pltfm[7] == ltr and pltfm[8] == ltr and pltfm[9] == ltr) \
+           or (pltfm[4] == ltr and pltfm[5] == ltr and pltfm[6] == ltr) \
+           or (pltfm[1] == ltr and pltfm[2] == ltr and pltfm[3] == ltr) \
+           or (pltfm[1] == ltr and pltfm[4] == ltr and pltfm[7] == ltr) \
+           or (pltfm[2] == ltr and pltfm[5] == ltr and pltfm[8] == ltr) \
+           or (pltfm[3] == ltr and pltfm[6] == ltr and pltfm[9] == ltr) \
+           or (pltfm[1] == ltr and pltfm[5] == ltr and pltfm[9] == ltr) \
+           or (pltfm[3] == ltr and pltfm[5] == ltr and pltfm[7] == ltr)
+
+
+def movjogador():
     pass
 
-def compMove():
+
+def compomve():
     pass
 
-def selecionaRandom(board):
+
+def selecionarandom(plataforma):
     pass
 
-def plataformaFull(board):
+
+def plataformafull(plataforma):
     pass
+
 
 def main():
     pass
+
 
 main()
